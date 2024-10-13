1dee5a11969398fdee07d08dba95b8896db0bd02
Spicetrails <cpcamera04@gmail.com>
2024-10-13 12:43:34 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('news-box').addEventListener('click', function() {
+  window.location.href = 'https://your-news-page-url.com';
+});
+
+document.getElementById('reviews-box').addEventListener('click', function() {
+  window.location.href = 'https://your-reviews-page-url.com';
+});
